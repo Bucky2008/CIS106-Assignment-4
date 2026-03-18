5f372f187a55c1fa0b4cc0572bf6f3a078453135
Bucky2008 <kk43743@mail.harpercollege.edu>
2026-03-17 23:47:00 -0500

    Add files via upload

---
+++ PS4P2.py
@@ -0,0 +1,17 @@
+#input phase 
+item = input("Enter item:")
+quantity = int(input("Enter quantity:"))
+
+#process phase
+if item == "A":
+   unit_price = 10.00
+else:
+   unit_price = 20.00
+
+extended_price = quantity * unit_price
+
+#output phase
+print("Item:", item)
+print("Quantity:", quantity)
+print("Unit price:", unit_price)
+print("Extended price:", extended_price)
